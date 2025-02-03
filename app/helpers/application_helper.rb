5761985a654277c37db4d22a8b826e39977a9848
Kirill Nasyrov <knasyrov@gmail>
2025-02-03 11:34:56 +0500

    coin formatter move to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,22 @@ module ApplicationHelper
     client = CoingeckoRuby::Client.new
     client.price(crypto, currency: :usd).try(:[], crypto).try(:[], 'usd') || 0.0
   end
+
+  def formatted_coins(wallets)
+    sum = 0.0
+    entries = wallets.map do |w|
+      price = crypto_to_usd(w.eid)
+      total = price * (w&.value || 0.0)
+      sum += total
+      {
+        object: w,
+        formatted_price: number_to_currency(price, precision: 6),
+        formatted_total: number_to_currency(total)
+      }
+    end
+    {
+      entries: entries,
+      total: number_to_currency(sum)
+    }
+  end
 end
